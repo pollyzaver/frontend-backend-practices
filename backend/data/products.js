810500aefa594fd12414242a73c2d96e696ff4c7
Полина Завершинская <polina@Polinas-MacBook-Air.local>
2026-02-27 12:01:29 +0300

    final changes

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,5 +1,4 @@
 module.exports = [
-<<<<<<< HEAD
   { 
     id: 1, 
     title: "The Beatles - Abbey Road", 
@@ -81,9 +80,3 @@ module.exports = [
     stock: 10
   }
 ];
-=======
-  { id: 1, title: "Кофе", price: 199, category: "Напитки" },
-  { id: 2, title: "Чай", price: 149, category: "Напитки" },
-  { id: 3, title: "Шоколад", price: 129, category: "Сладости" }
-];
->>>>>>> origin/main
